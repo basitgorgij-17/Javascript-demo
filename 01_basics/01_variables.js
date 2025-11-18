f3d1b6cd085489c6119c72ed4bc186c301b1d3f0
Abdul Basit <basitgorgij@gmail.com>
2025-11-18 12:32:05 +0000

    Add variables File

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountID = 123141
+let accountEmail = "abdulbasit@gmail.com"
+var accountPassword = "ab123"
+accountCity = "Karachi"
+
+// accountID = 08889
+
+accountEmail = "basoo@gmail.com"
+accountPassword = "ab17"
+accountCity = "Balochistan"
+
+console.log([accountID]);
+
+g
+console.table([accountID, accountEmail, accountPassword, accountCity])
