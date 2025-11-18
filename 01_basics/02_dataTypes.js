5a3042f4878acd1bf040d57d7ccaba211faf2e4a
Abdul Basit <basitgorgij@gmail.com>
2025-11-18 14:18:04 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //Treat all js code newer version
+/*
+Understand Data Types
+1.string => "Basit"
+2.number =>  17
+3.boolean => true/false
+4.null => empty value => standalone value
+5.let state; => undefined value => undefined is dataType
+*/
+
+console.log(6 + 6)
+let name = "Basit" //this is string
+let age = 17 //this is number
+let isloggedIn = false //this is boolean
+let state; //this is undefined value
+
+// Symbol => use for unique
+
+console.table([name, age, isloggedIn, state])
+
+//Object
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
+
+
+
+
