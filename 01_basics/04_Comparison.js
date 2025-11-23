0003b81f7a49433923d8f18fac1a4d016977bf5e
Abdul Basit <basitgorgij@gmail.com>
2025-11-23 17:02:18 +0000

    Understand Comparison

---
+++ 01_basics/04_Comparison.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// Avoid This Type Comparison
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+console.log(undefined < 2)
+
+
+// This Comparison ==== Check Value and Value Data Types
+console.log("2" === 2)
+
+
+
+
+
